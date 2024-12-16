10012c6a52810524d63fc5ea5de045eaf5615bc1
yaqoob2ishaq4 <yaqoob24ishaq@gmail.com>
2024-12-17 01:28:05 +0500

    change of padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import OverView from "./pages/overview/page";
 
 export default function DashboardPage() {
   return (
-    <div className="flex flex-1 items-center justify-center p-8">
+    <div className="flex flex-1 items-center justify-center">
       <OverView />
     </div>
   );
