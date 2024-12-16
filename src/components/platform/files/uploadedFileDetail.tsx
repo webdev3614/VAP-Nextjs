463d0ce86f40c3a17d729646ac7b08da3b1b37c0
yaqoob2ishaq4 <yaqoob24ishaq@gmail.com>
2024-12-16 12:38:54 +0500

    console isssue fixes

--- src/components/platform/files/uploadedFileDetail.tsx
+++ src/components/platform/files/uploadedFileDetail.tsx
@@ -91,7 +91,7 @@ const UploadedFileDetails: React.FC<UploadedFileDetailsProps> = ({
                         viewBox="0 0 29.375 27.2754"
                         fill="'none' || '#000000"
                         stroke="none"
-                        stroke-width="2"
+                        strokeWidth="2"
                         strokeLinecap="round"
                         strokeLinejoin="round"
                         className="w-12 h-12 mb-2 fill-icon/30 group-hover:fill-icon/40 fill-[#ffffff4d] hover:fill-[#ffffff66]"
@@ -134,7 +134,7 @@ const UploadedFileDetails: React.FC<UploadedFileDetailsProps> = ({
                         viewBox="0 0 20.2832 19.9316"
                         fill="'none' || '#000000"
                         stroke="none"
-                        stroke-width="2"
+                        strokeWidth="2"
                         strokeLinecap="round"
                         strokeLinejoin="round"
                         className="fill-white/50 group-hover:fill-white/70 w-[16px] h-[16px] group-hover:rotate-90 transition-all duration-150 ease-in-out"
@@ -206,7 +206,7 @@ const UploadedFileDetails: React.FC<UploadedFileDetailsProps> = ({
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
+                  strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   className="lucide lucide-chevron-down ml-2 h-4 w-4 shrink-0 opacity-50"
@@ -245,7 +245,7 @@ const UploadedFileDetails: React.FC<UploadedFileDetailsProps> = ({
                       <svg
                         stroke="currentColor"
                         fill="currentColor"
-                        stroke-width="0"
+                        strokeWidth="0"
                         viewBox="0 0 24 24"
                         className="text-8xl text-text/60 text-[#ffffff4d] hover:text-[#ffffff66]"
                         height="1em"
@@ -283,8 +283,8 @@ const UploadedFileDetails: React.FC<UploadedFileDetailsProps> = ({
                           <path
                             d="M7.50005 1.04999C7.74858 1.04999 7.95005 1.25146 7.95005 1.49999V8.41359L10.1819 6.18179C10.3576 6.00605 10.6425 6.00605 10.8182 6.18179C10.994 6.35753 10.994 6.64245 10.8182 6.81819L7.81825 9.81819C7.64251 9.99392 7.35759 9.99392 7.18185 9.81819L4.18185 6.81819C4.00611 6.64245 4.00611 6.35753 4.18185 6.18179C4.35759 6.00605 4.64251 6.00605 4.81825 6.18179L7.05005 8.41359V1.49999C7.05005 1.25146 7.25152 1.04999 7.50005 1.04999ZM2.5 10C2.77614 10 3 10.2239 3 10.5V12C3 12.5539 3.44565 13 3.99635 13H11.0012C11.5529 13 12 12.5528 12 12V10.5C12 10.2239 12.2239 10 12.5 10C12.7761 10 13 10.2239 13 10.5V12C13 13.1041 12.1062 14 11.0012 14H3.99635C2.89019 14 2 13.103 2 12V10.5C2 10.2239 2.22386 10 2.5 10Z"
                             fill="currentColor"
-                            fill-rule="evenodd"
-                            clip-rule="evenodd"
+                            fillRule="evenodd"
+                            clipRule="evenodd"
                           ></path>
                         </svg>{" "}
                         Download
@@ -301,8 +301,8 @@ const UploadedFileDetails: React.FC<UploadedFileDetailsProps> = ({
                           <path
                             d="M1 9.50006C1 10.3285 1.67157 11.0001 2.5 11.0001H4L4 10.0001H2.5C2.22386 10.0001 2 9.7762 2 9.50006L2 2.50006C2 2.22392 2.22386 2.00006 2.5 2.00006L9.5 2.00006C9.77614 2.00006 10 2.22392 10 2.50006V4.00002H5.5C4.67158 4.00002 4 4.67159 4 5.50002V12.5C4 13.3284 4.67158 14 5.5 14H12.5C13.3284 14 14 13.3284 14 12.5V5.50002C14 4.67159 13.3284 4.00002 12.5 4.00002H11V2.50006C11 1.67163 10.3284 1.00006 9.5 1.00006H2.5C1.67157 1.00006 1 1.67163 1 2.50006V9.50006ZM5 5.50002C5 5.22388 5.22386 5.00002 5.5 5.00002H12.5C12.7761 5.00002 13 5.22388 13 5.50002V12.5C13 12.7762 12.7761 13 12.5 13H5.5C5.22386 13 5 12.7762 5 12.5V5.50002Z"
                             fill="currentColor"
-                            fill-rule="evenodd"
-                            clip-rule="evenodd"
+                            fillRule="evenodd"
+                            clipRule="evenodd"
                           ></path>
                         </svg>{" "}
                         Copy URL{" "}
@@ -319,8 +319,8 @@ const UploadedFileDetails: React.FC<UploadedFileDetailsProps> = ({
                           <path
                             d="M5.5 1C5.22386 1 5 1.22386 5 1.5C5 1.77614 5.22386 2 5.5 2H9.5C9.77614 2 10 1.77614 10 1.5C10 1.22386 9.77614 1 9.5 1H5.5ZM3 3.5C3 3.22386 3.22386 3 3.5 3H5H10H11.5C11.7761 3 12 3.22386 12 3.5C12 3.77614 11.7761 4 11.5 4H11V12C11 12.5523 10.5523 13 10 13H5C4.44772 13 4 12.5523 4 12V4L3.5 4C3.22386 4 3 3.77614 3 3.5ZM5 4H10V12H5V4Z"
                             fill="currentColor"
-                            fill-rule="evenodd"
-                            clip-rule="evenodd"
+                            fillRule="evenodd"
+                            clipRule="evenodd"
                           ></path>
                         </svg>{" "}
                         Delete{" "}
@@ -357,7 +357,7 @@ const UploadedFileDetails: React.FC<UploadedFileDetailsProps> = ({
                           viewBox="0 0 21.0254 20.4785"
                           fill="'none' || '#000000"
                           stroke="none"
-                          stroke-width="2"
+                          strokeWidth="2"
                           strokeLinecap="round"
                           strokeLinejoin="round"
                           className="w-[14px] h-[14px] fill-[#ffffff4d] hover:fill-[#ffffff66]"
